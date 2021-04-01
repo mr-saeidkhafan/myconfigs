81f46450352c95fc3620d26798226cf6df3e609c
saeid-rasoli <saeidrasoli1212@gmail.com>
2021-04-02 03:59:23 +0430

    new screen recorder added

---
+++ ffmpeg_recorder_with_sound.sh
@@ -0,0 +1,2 @@
+notify-send "recording with sound..." "now it's recording the whole screen" -i /usr/share/icons/Papirus-Dark/16x16/actions/record.svg
+ffmpeg -video_size 1366x768 -framerate 30 -f x11grab -i $DISPLAY -f pulse -ac 2 -i default -acodec libmp3lame -ab 64k $HOME/output-$(date '+%Y-%m-%d_%R').mp4; notify-send "recording stoped" "the video saved in your home dir" -i /usr/share/icons/Papirus-Dark/16x16/actions/checkbox.svg
